cd13dbfbe802b4ff61dbea5516e9a650e28c5e8b
garret1317 <garret@airmail.cc>
2025-11-23 02:28:08 +0000

    change layout of plugin bundle

--- contrib/bundle.sh
+++ contrib/bundle.sh
@@ -4,12 +4,10 @@ mkdir bundle/
 uv pip install --python-version 3.9 --python-platform linux --requirements pyproject.toml --target bundle/yt_dlp_plugins/
 uv pip install --python-version 3.9 --python-platform linux --no-deps --target bundle/ .
 
+mkdir bundle/licenses/
 for lib_distinfo in bundle/yt_dlp_plugins/*.dist-info bundle/*.dist-info; do
 	lib=$(basename "$lib_distinfo" .dist-info)
-	for licence_file in "$lib_distinfo"/licenses/*; do
-		licence_filename=$(basename "$licence_file")
-		cp "${licence_file}" "bundle/${lib}_${licence_filename}"
-	done
+	cp -r "${lib_distinfo}/licenses" "bundle/licenses/${lib}"
 done
 
 rm -rf bundle/yt_dlp_plugins/*.dist-info bundle/*.dist-info bundle/yt_dlp_plugins/bin
